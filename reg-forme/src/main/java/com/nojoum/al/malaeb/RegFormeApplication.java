72f242c47250bb90fd12ef43f09173283afe70c0
Utilisateur <Utilisateur@DESKTOP-Q76N1AH>
2018-01-02 15:39:29 +0100

    First commit

---
+++ reg-forme/src/main/java/com/nojoum/al/malaeb/RegFormeApplication.java
@@ -0,0 +1,12 @@
+package com.nojoum.al.malaeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegFormeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegFormeApplication.class, args);
+	}
+}
